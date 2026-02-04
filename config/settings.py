ac03496c555b13fc71fac40e666fca4e8b54967b
Lenner Giraldo <arturogiraldo33@hotmail.com>
2026-02-04 11:57:42 -0500

    Fix ALLOWED_HOSTS for production

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=irqx!@7#buf%o07yq54dahtop4*edqua&zvs)fwc5bg(3p-2d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['104.145.210.2', 'localhost', '127.0.0.1']
 
 
 # Application definition
