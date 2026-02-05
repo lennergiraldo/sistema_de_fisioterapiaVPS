f075088299a70f5589db1cbd6281c9f2f69e68b7
Lenner Giraldo <arturogiraldo33@hotmail.com>
2026-02-05 15:26:46 -0500

    agregar

--- config/settings.py
+++ config/settings.py
@@ -77,16 +77,29 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'fisio',
-        'USER': 'django_user',
-        'PASSWORD': 'Django2026Seguro!',
-        'HOST': 'localhost',
-        'PORT': '3306',
+ENVIRONMENT = os.getenv("DJANGO_ENV", "development")
+
+if ENVIRONMENT == "production":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": "fisio",
+            "USER": "django_user",
+            "PASSWORD": "Django2026Seguro!",
+            "HOST": "localhost",
+            "PORT": "3306",
+            "OPTIONS": {
+                "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
+            }
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
