9d4e1199adf48dee78261149d875704f94b31510
Lenner Giraldo <arturogiraldo33@hotmail.com>
2026-02-17 12:02:07 -0500

    Corrigiendo STATIC_ROOT y activando WhiteNoise

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'core','static'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
